35a25efc301e4ef9d2c6c8a07896684f782db7e4
Devin <devinpquinn4@gmail.com>
2021-10-19 14:50:04 -0400

    basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour
 
     public playerState myState;
 
+    private Transform body;
     private Rigidbody2D rb_body;
+    private Animator rb_anim;
 
     public float moveSpeed = 5f;
     Vector2 movement;
@@ -16,13 +18,28 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         myState = playerState.Body;
-        rb_body = transform.Find("BodySprite").gameObject.GetComponent<Rigidbody2D>();
+        body = transform.Find("BodySprite");
+        rb_body = body.GetComponent<Rigidbody2D>();
+        rb_anim = body.GetComponent<Animator>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(myState == playerState.Body)
+        {
+            rb_anim.SetFloat("Speed", movement.sqrMagnitude);
+            if(movement.x < 0)
+            {
+                body.localScale = new Vector3(1, body.localScale.y, body.localScale.z);
+            }
+            else if (movement.x > 0)
+            {
+                body.localScale = new Vector3(-1, body.localScale.y, body.localScale.z);
+            }
+        }
 
         if(movement.x != 0 && movement.y != 0) 
         {
