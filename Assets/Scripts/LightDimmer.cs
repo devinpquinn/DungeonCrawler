4a1f5e45db438d3e2760fa630cc2a2e0726cffe3
Devin <devinpquinn4@gmail.com>
2021-11-04 14:06:29 -0400

    layer sorting

---
+++ Assets/Scripts/LightDimmer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightDimmer : MonoBehaviour
+{
+    private Light2D l;
+    public float intensity;
+
+    private void Awake()
+    {
+        l = GetComponent<Light2D>();
+        l.intensity = intensity;
+    }
+}
