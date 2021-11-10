6e659fc30e7012506f58da750daab8745c04eab2
Devin <devinpquinn4@gmail.com>
2021-11-10 13:35:16 -0500

    setting up custom cursor

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Texture2D mouseDefault;
+
+    private void Awake()
+    {
+        Cursor.SetCursor(mouseDefault, new Vector2(0, 0), CursorMode.Auto);
+    }
+}
