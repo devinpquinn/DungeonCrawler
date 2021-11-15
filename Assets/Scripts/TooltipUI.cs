d62388afc2b44c5ff215c2f0b9ed623254233a71
Devin <devinpquinn4@gmail.com>
2021-11-15 17:53:06 -0500

    working on tooltip

---
+++ Assets/Scripts/TooltipUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TooltipUI : MonoBehaviour
+{
+    private RectTransform backgroundRectTransform;
+    private TextMeshProUGUI tmp;
+
+    private void Awake()
+    {
+        backgroundRectTransform = transform.Find("Background").GetComponent<RectTransform>();
+        tmp = transform.Find("Text").GetComponent<TextMeshProUGUI>();
+        SetText("Wooden Door");
+    }
+
+    private void SetText(string tooltipText)
+    {
+        tmp.SetText(tooltipText);
+        tmp.ForceMeshUpdate();
+
+        Vector2 textSize = tmp.GetRenderedValues(false);
+        Vector2 paddingSize = new Vector2(24, 24);
+
+        backgroundRectTransform.sizeDelta = textSize + paddingSize;
+    }
+}
