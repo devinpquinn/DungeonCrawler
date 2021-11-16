ac9ee62b5391362ae7fa07afa5b3d135a97ce113
Devin <devinpquinn4@gmail.com>
2021-11-15 19:45:53 -0500

    dialogue panel setup

---
+++ Assets/Scripts/UIHeadManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHeadManager : MonoBehaviour
+{
+    private Image image;
+    public SpriteRenderer targetSprite;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void FixedUpdate()
+    {
+        image.sprite = targetSprite.sprite;
+    }
+
+}
