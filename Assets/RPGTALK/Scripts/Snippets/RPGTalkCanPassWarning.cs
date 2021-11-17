f3f3354256edebd83d680728210bc5eb112fedb2
Devin <devinpquinn4@gmail.com>
2021-11-16 20:37:22 -0500

    imported rpgtalk

---
+++ Assets/RPGTALK/Scripts/Snippets/RPGTalkCanPassWarning.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RPGTALK.Snippets
+{
+    [AddComponentMenu("Seize Studios/RPGTalk/Snippets/Can Pass Warning")]
+    public class RPGTalkCanPassWarning : MonoBehaviour
+    {
+
+        public UnityEvent OnCanPass, OnPassed;
+
+        RPGTalk rpgtalk;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rpgtalk = GetComponent<RPGTalk>();
+            rpgtalk.OnEndAnimating += CanPass;
+            rpgtalk.OnPlayNext += Passed;
+            rpgtalk.OnEndTalk += Passed;
+        }
+
+        void CanPass()
+        {
+            StartCoroutine(CheckPass());
+        }
+
+        IEnumerator CheckPass()
+        {
+            yield return new WaitForEndOfFrame();
+            if (rpgtalk.enablePass)
+            {
+                OnCanPass.Invoke();
+            }
+        }
+
+        void Passed()
+        {
+            OnPassed.Invoke();
+        }
+    }
+}
