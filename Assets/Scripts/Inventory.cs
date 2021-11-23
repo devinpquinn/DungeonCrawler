31d0b67aa7889b7bd8d01746fb4930acb5cdaf69
Devin <devinpquinn4@gmail.com>
2021-11-23 14:30:36 -0500

    inventory work

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> items;
+    public Transform contentHolder;
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Sprite itemSprite;
+    public string itemName;
+    [TextArea]
+    public string itemDescription;
+}
