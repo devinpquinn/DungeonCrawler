59f85d184d243b2c2643dab66ff38abc25e6a9a6
Devin <devinpquinn4@gmail.com>
2021-12-29 13:35:29 -0500

    early follow cam

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform player;
+	public Vector3 offset = new Vector3(0, 1, -10);
+	public float speed = 1;
+
+	void Update()
+	{
+		Vector3 currentPosition = transform.position;
+		Vector3 targetPosition = player.position + offset;
+		transform.position = Vector3.Lerp(currentPosition, targetPosition, Time.deltaTime * speed);
+	}
+}
