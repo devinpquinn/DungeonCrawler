017aee35146a4c9991ab45b4bfee7295938c842a
Devin <devinpquinn4@gmail.com>
2021-12-29 13:55:30 -0500

    camera bounding

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,10 +8,20 @@ public class CameraFollow : MonoBehaviour
 	public Vector3 offset = new Vector3(0, 1, -10);
 	public float speed = 1;
 
-	void Update()
+	[SerializeField] CameraBounds2D bounds;
+	Vector2 maxXPositions, maxYPositions;
+
+    private void Awake()
+    {
+		bounds.Initialize(GetComponent<Camera>());
+		maxXPositions = bounds.maxXlimit;
+		maxYPositions = bounds.maxYlimit;
+	}
+
+    void Update()
 	{
 		Vector3 currentPosition = transform.position;
-		Vector3 targetPosition = player.position + offset;
+		Vector3 targetPosition = new Vector3(Mathf.Clamp(player.position.x, maxXPositions.x, maxXPositions.y), Mathf.Clamp(player.position.y, maxYPositions.x, maxYPositions.y), currentPosition.z) + offset;
 		transform.position = Vector3.Lerp(currentPosition, targetPosition, Time.deltaTime * speed);
 	}
 }
