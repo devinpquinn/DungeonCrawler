c798ddc929905ec911a07e1c3ecab8a22a6a4631
Devin <devinpquinn4@gmail.com>
2022-01-21 21:25:53 -0500

    added snap sounds

---
+++ Assets/Scripts/SnapSounds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapSounds : MonoBehaviour
+{
+    public AudioSource snapSource;
+
+    public List<AudioClip> snapSounds;
+
+    public void PlaySnapSound()
+    {
+        int key = Random.Range(0, snapSounds.Count);
+        snapSource.PlayOneShot(snapSounds[key]);
+    }
+}
