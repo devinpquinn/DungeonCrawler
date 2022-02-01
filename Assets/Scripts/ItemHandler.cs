0aea24e8339372e26ddf48f9691f3f2ea7a5b406
Devin <devinpquinn4@gmail.com>
2022-01-31 23:46:53 -0500

    equipping and unequipping sounds

--- Assets/Scripts/ItemHandler.cs
+++ Assets/Scripts/ItemHandler.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class ItemHandler : MonoBehaviour
 {
     public string myItemName;
+    private AudioSource myAudioSource;
+
+    public AudioClip equipSound;
+    public AudioClip unequipSound;
+
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
 
     public void EquipItem()
     {
         if(PlayerController.GetEquippedItem() == myItemName)
         {
             PlayerController.UnequipAllItems();
+            myAudioSource.PlayOneShot(unequipSound);
         }
         else
         {
             PlayerController.EquipItem(myItemName);
+            myAudioSource.PlayOneShot(equipSound);
         }
     }
 
