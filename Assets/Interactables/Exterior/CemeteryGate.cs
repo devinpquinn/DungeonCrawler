039062e562d309d37c499f67e75cf0b96fc7ff94
Devin <devinpquinn4@gmail.com>
2022-03-07 21:58:19 -0500

    gate work

---
+++ Assets/Interactables/Exterior/CemeteryGate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CemeteryGate : Interactable
+{
+    public SpriteRenderer gateSpriteRenderer;
+    public Sprite openGateSprite;
+    public Collider2D gateCollider;
+    public AudioSource gateAudioSource;
+
+    public override void Interact()
+    {
+        base.Interact();
+        myTalk.NewTalk("gate-first");
+    }
+
+    public override void DoEvent(string key)
+    {
+        if(key == "checkForKey")
+        {
+            if(PlayerController.GetEquippedItem() != null)
+            {
+                myTalk.NewTalk("gate-item");
+            }
+            else
+            {
+                myTalk.NewTalk("gate-no-item");
+            }
+        }
+        else if(key == "openGate")
+        {
+            gateSpriteRenderer.sprite = openGateSprite;
+            gateCollider.enabled = false;
+            gateAudioSource.Play();
+            PlayerController.RemoveItem("Iron Key");
+            this.gameObject.layer = 0;
+        }
+    }
+}
