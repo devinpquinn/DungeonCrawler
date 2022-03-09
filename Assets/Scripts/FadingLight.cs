0ff3cbdfe980325b184d4160e843d282d18e6fc1
Devin <devinpquinn4@gmail.com>
2022-03-09 00:32:23 -0500

    sprite update, dialogue edits, fading shadows

--- Assets/Scripts/FadingLight.cs
+++ Assets/Scripts/FadingLight.cs
@@ -7,6 +7,7 @@ public class FadingLight : MonoBehaviour
 {
     private Light2D myLight;
     float startingIntensity;
+    float startingShadow;
     public float fadeDuration = 1f;
     float elapsed = 0f;
 
@@ -14,11 +15,13 @@ public class FadingLight : MonoBehaviour
     {
         myLight = GetComponent<Light2D>();
         startingIntensity = myLight.intensity;
+        startingShadow = myLight.shadowIntensity;
     }
 
     private void Update()
     {
         myLight.intensity = Mathf.Lerp(startingIntensity, 0, elapsed / fadeDuration);
+        myLight.shadowIntensity = Mathf.Lerp(startingShadow, 0, elapsed / fadeDuration);
         elapsed += Time.deltaTime;
         if (elapsed >= fadeDuration)
         {
