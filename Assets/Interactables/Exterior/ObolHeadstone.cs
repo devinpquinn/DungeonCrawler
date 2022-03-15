76fbce43528f8b4b7c5ee7f1c00c4c7c7671200d
Devin <devinpquinn4@gmail.com>
2022-03-14 21:03:35 -0400

    obol headstone

---
+++ Assets/Interactables/Exterior/ObolHeadstone.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObolHeadstone : Interactable
+{
+    public GameObject trapdoorHole;
+
+    public bool CheckEquipped()
+    {
+        string itemCheck = PlayerController.GetEquippedItem();
+        Debug.Log(itemCheck);
+        if (itemCheck == "Unbreakable Knot" || itemCheck == "Scrawled Notes" || itemCheck == "Hateful Shovel")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        if (interactedWith && CheckEquipped())
+        {
+            myTalk.NewTalk("obol");
+        }
+        else
+        {
+            myTalk.NewTalk("obol-first");
+            interactedWith = true;
+        }
+    }
+
+    public override void DoEvent(string key)
+    {
+        if (key == "checkObol")
+        {
+            if (CheckEquipped())
+            {
+                myTalk.NewTalk("obol");
+            }
+            else
+            {
+                myTalk.EndTalk();
+            }
+        }
+        else if (key == "dug")
+        {
+            trapdoorHole.SetActive(true);
+            gameObject.layer = 0;
+        }
+    }
+}
