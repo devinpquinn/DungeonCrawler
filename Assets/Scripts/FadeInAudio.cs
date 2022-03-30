ce8a88f23b20968c805d05e323fb822dca75389c
Devin <devinpquinn4@gmail.com>
2022-03-30 19:51:24 -0400

    audio fade in

---
+++ Assets/Scripts/FadeInAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInAudio : MonoBehaviour
+{
+    [Range(0.1f, 1f)]
+    public float targetVolume = 1f;
+
+    private AudioSource src;
+
+    private void Awake()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        FadeInSound();
+    }
+
+    public void FadeInSound()
+    {
+        src.volume = 0f;
+        StartCoroutine(DoFadeInSound());
+    }
+
+    IEnumerator DoFadeInSound()
+    {
+        while (src.volume < targetVolume)
+        {
+            src.volume += Time.deltaTime;
+            yield return null;
+        }
+        src.volume = targetVolume;
+        yield return null;
+    }
+}
