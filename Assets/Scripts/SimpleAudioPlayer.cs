631feac8b74bc48450ca56ef1fa2cd72a85db987
Devin <devinpquinn4@gmail.com>
2022-04-14 15:19:49 -0400

    pregame sound updates

---
+++ Assets/Scripts/SimpleAudioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAudioPlayer : MonoBehaviour
+{
+    private AudioSource src;
+    public AudioClip clip;
+
+    private void Awake()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        src.PlayOneShot(clip);
+    }
+}
