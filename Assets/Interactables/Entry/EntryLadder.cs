4dabc53631030467f5be7a28021c8d17411f81f8
Devin <devinpquinn4@gmail.com>
2022-04-29 11:07:34 -0400

    entry interactables

---
+++ Assets/Interactables/Entry/EntryLadder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryLadder : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+        if (interactedWith)
+        {
+            myTalk.NewTalk("ladder-again");
+        }
+        else
+        {
+            myTalk.NewTalk("ladder");
+            interactedWith = true;
+        }
+    }
+}
---
+++ Assets/Interactables/Entry/EntryStorage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryStorage : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+        if (interactedWith)
+        {
+            myTalk.NewTalk("storage-again");
+        }
+        else
+        {
+            myTalk.NewTalk("storage");
+            interactedWith = true;
+        }
+    }
+}
