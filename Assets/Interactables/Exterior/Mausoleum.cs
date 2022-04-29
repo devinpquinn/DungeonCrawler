f5d26e3a49f0cd3e97973efba27e7af31ae6153b
Devin <devinpquinn4@gmail.com>
2022-04-28 23:25:40 -0400

    mausoleum update

--- Assets/Interactables/Exterior/Mausoleum.cs
+++ Assets/Interactables/Exterior/Mausoleum.cs
@@ -7,13 +7,22 @@ public class Mausoleum : Interactable
     public override void Interact()
     {
         base.Interact();
-        myTalk.NewTalk("maus");
+        if (interactedWith)
+        {
+            myTalk.NewTalk("maus-again");
+        }
+        else
+        {
+            myTalk.NewTalk("maus");
+            interactedWith = true;
+        }
     }
 
     public override void DoEvent(string key)
     {
         if(key == "checkMaus")
         {
+            gameObject.name = "Mouse-oleum";
             if(PlayerController.GetEquippedItem() != null && PlayerController.GetEquippedItem() == "Unbreakable Knot")
             {
                 myTalk.NewTalk("maus-q");
