d7d4a3b2c09d1015c6f4e7dd2f0ad0323e024aa7
Devin <devinpquinn4@gmail.com>
2022-07-17 15:47:11 -0400

    playtest inventory

---
+++ Assets/Scripts/PlaytestInventory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaytestInventory : MonoBehaviour
+{
+    public List<ItemObject> items;
+
+    private void Start()
+    {
+        foreach(ItemObject i in items)
+        {
+            PlayerController.AddItem(i);
+        }
+    }
+}
